906b250ffe7a67d1b5b58c6bb00e4a7da4607f45
vishal07316 <vishalkumarv2020@gmail.com>
2024-02-15 20:32:30 +0530

    change images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -68,7 +68,7 @@ export default function Header() {
         <Link to="/">
           <EuiText>
             <h2 style={{ padding: "0 1vw" }}>
-              <EuiTextColor color="#0b5cff">Zoom</EuiTextColor>
+              <EuiTextColor color="#0b5cff">UVSyncTalk</EuiTextColor>
             </h2>
           </EuiText>
         </Link>,
@@ -139,7 +139,7 @@ export default function Header() {
         <Link to="/">
           <EuiText>
             <h2 style={{ padding: "0 1vw" }}>
-              <EuiTextColor color="#0b5cff">Zoom</EuiTextColor>
+              <EuiTextColor color="#0b5cff">UVSyncTalk</EuiTextColor>
             </h2>
           </EuiText>
         </Link>,
--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,15 +1,6 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: "react-auth-6788d.firebaseapp.com",
-//   projectId: "react-auth-6788d",
-//   storageBucket: "react-auth-6788d.appspot.com",
-//   messagingSenderId: "131797845021",
-//   appId: "1:131797845021:web:3f7ff4766e2b89ca5d32f4",
-//   measurementId: "G-VWPBR1NSLL",
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyCkEIyU1iuV5EesjHYxLH7__cQhkXljoUg",
